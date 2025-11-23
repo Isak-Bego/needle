1de664c72151bb438d369069aeee374db066abfb
Isak Bego <isakbego@gmail.com>
2025-11-23 16:57:04 +0200

    add tests for the auto-diff engine

--- tests/unit/test_autoGradEngine.cpp
+++ tests/unit/test_autoGradEngine.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <autoGradEngine/node.h>
+#include <cmath>
 
 TEST(AutoGradEngine, AdditionForward) {
     //Given
@@ -73,5 +74,82 @@ TEST(AutoGradEngine, LogarithmBackward) {
     //Then
     EXPECT_NEAR(f->data, std::log(2.0), 1e-7);
     EXPECT_NEAR(x.grad, 0.5, 1e-7);
+    delete f;
+}
+
+TEST(AutoGradEngine, DivisionNodeNode) {
+    //Given
+    Node a(10.0);
+    Node b(2.0);
+
+    //When
+    Node *c = a / b;
+    c->backward();
+
+    //Then
+    EXPECT_DOUBLE_EQ(c->data, 5.0);
+    EXPECT_DOUBLE_EQ(a.grad, 0.5); // 1/2
+    EXPECT_DOUBLE_EQ(b.grad, -2.5); // -10/4
+    delete c;
+}
+
+TEST(AutoGradEngine, DivisionNodeScalar) {
+    //Given
+    Node a(10.0);
+
+    //When
+    Node *c = a / 2.0;
+    c->backward();
+
+    //Then
+    EXPECT_DOUBLE_EQ(c->data, 5.0);
+    EXPECT_DOUBLE_EQ(a.grad, 0.5);
+    delete c;
+}
+
+TEST(AutoGradEngine, DivisionScalarNode) {
+    //Given
+    Node b(2.0);
+
+    //When
+    Node *c = 10.0 / b;
+    c->backward();
+
+    //Then
+    EXPECT_DOUBLE_EQ(c->data, 5.0);
+    EXPECT_DOUBLE_EQ(b.grad, -2.5);
+    delete c;
+}
+
+TEST(AutoGradEngine, PowerOperation) {
+    //Given
+    Node x(4.0);
+    double exponent = 3.0;
+
+    //When
+    Node *f = x.pow(exponent);
+    f->backward();
+
+    //Then
+    EXPECT_DOUBLE_EQ(f->data, 64.0);
+    EXPECT_DOUBLE_EQ(x.grad, 48.0);
+    delete f;
+}
+
+TEST(AutoGradEngine, ChainRuleComplex) {
+    //Given
+    Node x(3.0);
+    Node one(1.0);
+
+    //When
+    Node *u = x + one;
+    Node *f = u->pow(2.0);
+    f->backward();
+
+    //Then
+    EXPECT_DOUBLE_EQ(f->data, 16.0); // 4^2
+    EXPECT_DOUBLE_EQ(x.grad, 8.0);   // 2 * 4
+
+    delete u;
     delete f;
 }
